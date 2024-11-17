7ba6c312ed854533cddabe76d941e01fff02beae
Sonia Leena <sonialee@et-j5.kzn.21-school.ru>
2024-11-17 16:54:09 +0300

    добавил

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	// TODO : config : cleanenv
+
+	// TODO : logger : log/slog
+
+	// TODO : storage : sqlite
+
+	// TODO : router : chi, chi-render
+
+	// TODO : run server
+
+}
