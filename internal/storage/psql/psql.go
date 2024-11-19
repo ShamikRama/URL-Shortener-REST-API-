fb7f11749d8415fe01472455cbe5254afb6b78ee
Sonia Leena <sonialee@si-i5.kzn.21-school.ru>
2024-11-19 18:43:46 +0300

    добавил

--- internal/storage/psql/psql.go
+++ internal/storage/psql/psql.go
@@ -2,15 +2,19 @@ package psql
 
 import (
 	"Rest/internal/config"
+	"Rest/internal/storage"
 	"database/sql"
+	"errors"
 	"fmt"
 
-	_ "github.com/lib/pq"
+	"github.com/lib/pq"
 )
 
 type Storage struct {
 	db *sql.DB
 }
+
+var ErrConstraintCode = 23505
 
 func New(cfg config.Config) (*Storage, error) {
 	const op = "storage.psql.New"
@@ -49,4 +53,45 @@ func New(cfg config.Config) (*Storage, error) {
 	return &Storage{
 		db: db,
 	}, nil
+}
+
+func (s *Storage) SaveUrl(urlToSave string, alias string) (int64, error) {
+	const op = "storage.psql.SaveURL"
+
+	stmt, err := s.db.Prepare("INSERT INTO urls(url, alias) VALUES($1, $2)")
+	if err != nil {
+		return 0, fmt.Errorf("%s : %w", op, err)
+	}
+
+	var id int64
+	err = stmt.QueryRow(urlToSave, alias).Scan(&id)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok && pqErr.Code == "ErrConstraintCode" {
+			return 0, fmt.Errorf("%s : %w", op, err)
+		}
+		return 0, fmt.Errorf("%s : %w", op, err)
+	}
+
+	return id, nil
+}
+
+func (s *Storage) GetUrl(alias string) (string, error) {
+	const op = "storage.psql.GetURL"
+
+	stmt, err := s.db.Prepare("SELECT url FROM urls WHERE alias = ? )")
+	if err != nil {
+		return "", fmt.Errorf("%s : %w", op, err)
+	}
+
+	var resURL string
+
+	err = stmt.QueryRow(alias).Scan(&resURL)
+	if errors.Is(err, sql.ErrNoRows) {
+		return "", storage.ErrUrlNotFound
+	} else if err != nil {
+		return "", fmt.Errorf("%s : %w", op, err)
+	}
+
+	return resURL, nil
+
 }
